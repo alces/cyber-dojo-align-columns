7252ce2540fc5336aabadfd459425cc9ab6fb1b7
3K3tG1 <3K3tG1@cyber-dojo.org>
2024-06-17 07:33:40 +0000

    5 ran tests, predicted green, got green

--- files/align.go
+++ files/align.go
@@ -1,5 +1,13 @@
 package align
 
+import (
+    "strings"
+)
+
 func split(text string) (result [][]string) {
+    for _, l := range strings.Split(text, "\n") {
+        result = append(result, strings.Split(l, "$"))
+    }
+    
     return
 }
